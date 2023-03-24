7b459d9b3a06a041855b4085c9424d97e14cebab
mopheshi <ndachimya@gmail.com>
2023-03-25 00:23:38 +0100

    Day 11

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            one = new Label();
+            two = new Label();
+            three = new Label();
+            four = new Label();
+            five = new Label();
+            six = new Label();
+            pick = new Button();
+            reset = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // one
+            // 
+            one.AutoSize = true;
+            one.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            one.Location = new Point(528, 19);
+            one.Name = "one";
+            one.Size = new Size(0, 41);
+            one.TabIndex = 0;
+            // 
+            // two
+            // 
+            two.AutoSize = true;
+            two.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            two.Location = new Point(478, 19);
+            two.Name = "two";
+            two.Size = new Size(0, 41);
+            two.TabIndex = 1;
+            // 
+            // three
+            // 
+            three.AutoSize = true;
+            three.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            three.Location = new Point(428, 19);
+            three.Name = "three";
+            three.Size = new Size(0, 41);
+            three.TabIndex = 2;
+            // 
+            // four
+            // 
+            four.AutoSize = true;
+            four.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            four.Location = new Point(378, 19);
+            four.Name = "four";
+            four.Size = new Size(0, 41);
+            four.TabIndex = 3;
+            // 
+            // five
+            // 
+            five.AutoSize = true;
+            five.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            five.Location = new Point(328, 19);
+            five.Name = "five";
+            five.Size = new Size(0, 41);
+            five.TabIndex = 4;
+            // 
+            // six
+            // 
+            six.AutoSize = true;
+            six.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            six.Location = new Point(278, 19);
+            six.Name = "six";
+            six.Size = new Size(0, 41);
+            six.TabIndex = 5;
+            // 
+            // pick
+            // 
+            pick.AutoSize = true;
+            pick.Location = new Point(278, 147);
+            pick.Name = "pick";
+            pick.Size = new Size(170, 30);
+            pick.TabIndex = 6;
+            pick.Text = "Get My Lucky Numbers";
+            pick.UseVisualStyleBackColor = true;
+            pick.Click += pick_Click;
+            // 
+            // reset
+            // 
+            reset.Location = new Point(484, 148);
+            reset.Name = "reset";
+            reset.Size = new Size(94, 29);
+            reset.TabIndex = 7;
+            reset.Text = "Reset";
+            reset.UseVisualStyleBackColor = true;
+            reset.Click += reset_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(9, 9);
+            pictureBox1.Margin = new Padding(0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(250, 168);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(590, 190);
+            Controls.Add(pictureBox1);
+            Controls.Add(reset);
+            Controls.Add(pick);
+            Controls.Add(six);
+            Controls.Add(five);
+            Controls.Add(four);
+            Controls.Add(three);
+            Controls.Add(two);
+            Controls.Add(one);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Lotto Number Picker";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label one;
+        private Label two;
+        private Label three;
+        private Label four;
+        private Label five;
+        private Label six;
+        private Button pick;
+        private Button reset;
+        private PictureBox pictureBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,54 @@ namespace Lotto
         {
             InitializeComponent();
         }
+
+        // Clears all labels and sets buttons to default values on load
+        private void Clear()
+        {
+            one.Text = "...";
+            two.Text = "...";
+            three.Text = "...";
+            four.Text = "...";
+            five.Text = "...";
+            six.Text = "...";
+
+            pick.Enabled = true;
+            reset.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void pick_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int[] sequence = new int[60];   // Array to hold numebers 1 - 60
+
+            // Fill array elements 0 - 59 with numbers 1 - 60
+            for (int i = 1; i <= sequence.Length; i++) sequence[i - 1] = i;
+            for (int i = 0; i < sequence.Length; i++)   // Shuffle values in all array elements 
+            {
+                int j = (random.Next() % sequence.Length);
+                int k = sequence[i]; sequence[i] = sequence[j]; sequence[j] = k;
+            }
+
+            // Display values in array elements 1 - 6
+            one.Text = sequence[1].ToString();
+            two.Text = sequence[2].ToString();
+            three.Text = sequence[3].ToString();
+            four.Text = sequence[4].ToString();
+            five.Text = sequence[5].ToString();
+            six.Text = sequence[6].ToString();
+
+            pick.Enabled = false;
+            reset.Enabled = true;
+        }
     }
 }
